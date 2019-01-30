6885c28e0596a775db341609fccc337c20b8ea37
Yusuke <k.yusuke99@gmail.com>
2019-01-30 23:51:05 +0900

    tasksのviewまで作成

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Folder;
 
 class TaskController extends Controller
 {
     //
     public function index()
     {
-        return "Hello world";
+        $folders = Folder::all();
+
+        return view('tasks/index', [
+            'folders' => $folders,
+        ]);
     }
 }
