a02a7bfe27700c36b2b3c4f473864edf7d166c86
Yusuke <k.yusuke99@gmail.com>
2019-02-02 00:37:51 +0900

    フォルダ一覧表示まで

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -8,12 +8,13 @@ use App\Folder;
 class TaskController extends Controller
 {
     //
-    public function index()
+    public function index(int $id)
     {
         $folders = Folder::all();
 
         return view('tasks/index', [
             'folders' => $folders,
+            'current_folder_id' => $id,
         ]);
     }
 }
